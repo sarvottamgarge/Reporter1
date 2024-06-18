08c55c98af5a7db887e491f19a151fe9d2f039ee
Garge Sarvottam shripad <68537570+sarvottamgarge@users.noreply.github.com>
2024-06-18 11:02:24 +0530

    main.go

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -28,7 +28,7 @@ import (
 )
 
 var proto = flag.String("proto", "https://", "Grafana Protocol. Change to 'https://' if Grafana is using https. Reporter will still serve http.")
-var ip = flag.String("ip", "ngpmonitor.altius.cc:3000", "Grafana IP and port.")
+var ip = flag.String("ip", "ngpmonitoruat.altius.cc:3000", "Grafana IP and port.")
 var port = flag.String("port", ":8686", "Port to serve on.")
 var templateDir = flag.String("templates", "templates/", "Directory for custom TeX templates.")
 var sslCheck = flag.Bool("ssl-check", true, "Check the SSL issuer and validity. Set this to false if your Grafana serves https using an unverified, self-signed certificate.")
